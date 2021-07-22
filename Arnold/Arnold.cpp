6f897ceabbd03aeb2337c5af3638ab800386aede
Spectrum76 <49938052+Spectrum76@users.noreply.github.com>
2021-07-22 20:44:49 +0530

    Adding a Sphere.
    Creating Our First Raytraced Image.

--- Arnold/Arnold.cpp
+++ Arnold/Arnold.cpp
@@ -13,8 +13,20 @@
 
 #include "Ray.h"
 
+bool hit_sphere(const vec3& center, float radius, const Ray& r)
+{
+	vec3 oc = r.origin() - center;
+	auto a = dot(r.direction(), r.direction());
+	auto b = 2.0f * dot(oc, r.direction());
+	auto c = dot(oc, oc) - radius * radius;
+	auto discriminant = b * b - 4 * a * c;
+	return (discriminant > 0);
+}
+
 vec3 ray_color(const Ray& r)
 {
+	if (hit_sphere(vec3(0, 0, -1), 0.5, r))
+		return vec3(1, 0, 0);
 	vec3 unit_direction = normalize(r.direction());
 	auto t = 0.5f * (unit_direction.y + 1.0f);
 	return (1.0f - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
